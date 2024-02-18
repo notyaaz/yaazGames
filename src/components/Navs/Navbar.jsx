fdeb9b5ce61c0f018febb4cc1853500cb1da3644
Yazan Dalbah <dalbahyazan2@gmail.com>
2024-02-18 17:38:58 +0200

    try to fix image bug

--- src/components/Navs/Navbar.jsx
+++ src/components/Navs/Navbar.jsx
@@ -10,12 +10,21 @@ export default function Navbar() {
           className={`${styles.sectionPadding} flex justify-between items-center`}
         >
           <Link to={"/"}>
-            <img src="../../../assets/images/Logo.png" alt="" />
+            <img src="../../../public/images/Logo.png" alt="logo" />
           </Link>
           <div className="flex justify-center items-center gap-[10px] lg:gap-[30px] font-bold text-[1.25rem]">
-            <Link className="text-accent hover:text-accentHover" to={"/"}>Home</Link>
-            <Link className="text-accent hover:text-accentHover" to={"/offline"}>Singleplayer</Link>
-            <Link className="text-accent hover:text-accentHover" to={"/online"}>Multiplayer</Link>
+            <Link className="text-accent hover:text-accentHover" to={"/"}>
+              Home
+            </Link>
+            <Link
+              className="text-accent hover:text-accentHover"
+              to={"/offline"}
+            >
+              Singleplayer
+            </Link>
+            <Link className="text-accent hover:text-accentHover" to={"/online"}>
+              Multiplayer
+            </Link>
           </div>
         </nav>
       </div>
